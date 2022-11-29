46173eac5c85dc98b9601720b942504fd15e9b72
杰克有茶 <chenwansal1@163.com>
2022-11-29 22:09:04 +0800

    <feature> add: ECS - Hello World

--- main.rs
+++ main.rs
@@ -1,3 +1,41 @@
-fn main() {
+use bevy::prelude::*;
+
+// ==== Components ====
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+// ==== Systems ====
+fn sys_add_person(mut commands: Commands) {
+    commands.spawn((Person, Name("Jack".to_string())));
+}
+
+fn sys_hello() {
     println!("Hello, world!");
+}
+
+fn sys_greetings(query: Query<&Name>) {
+    for name in query.iter() {
+        println!("To: {}!", name.0);
+    }
+}
+
+// ==== Plugins ====
+pub struct  PluginHello;
+impl Plugin for PluginHello {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(sys_add_person)
+            .add_system(sys_hello)
+            .add_system(sys_greetings);
+    }
+}
+
+// ==== Main ====
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PluginHello)
+        .run();
 }
