bea6301a748cda34b03cd7e76ffd062d6938a562
杰克有茶 <chenwansal1@163.com>
2022-12-05 22:33:46 +0800

    <feature> add: Show Image

--- src/system/sys_init_load_asset.rs
+++ src/system/sys_init_load_asset.rs
@@ -1,3 +1,10 @@
-pub fn init() {
-    println!("Load Assets!");
+use bevy::prelude::*;
+
+pub fn init(mut command: Commands, assets_server: Res<AssetServer>) {
+    command.spawn(Camera2dBundle::default());
+    command.spawn(SpriteBundle {
+        texture: assets_server.load("icon/icon.png"),
+        transform: Transform::from_xyz(100.0, 0.0, 0.0),
+        ..Default::default()
+    });
 }
